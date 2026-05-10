a173c9573297b25b466ee085d8a28060528fc8d0
BaoPhuCNTT <phu.ndb.65cntt@ntu.edu.vn>
2026-05-11 02:10:38 +0700

    TodoList App P3

--- ViecCanLam/app/src/main/java/edu/phu/vieccanlam/MainActivity.java
+++ ViecCanLam/app/src/main/java/edu/phu/vieccanlam/MainActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,6 +21,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     List<TASKS> lstVCL;
+    TaskRVadapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,49 +32,56 @@ public class MainActivity extends AppCompatActivity {
 
         // Tạo danh sách
         lstVCL = new ArrayList<>();
-
         // Kết nối Firebase
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-        DatabaseReference databaseReference =
-                database.getReference("TASKS");
-
+        DatabaseReference databaseReference = database.getReference("TASKS");
         // Lắng nghe dữ liệu
-        databaseReference.addValueEventListener(
-                new ValueEventListener() {
+        ValueEventListener ngheFB;
+        databaseReference.addValueEventListener(ngheFB);
+        // Tìm dieu khien
+        RecyclerView recycleView = findViewById(R.id.rcvVCL);
+        LinerLayoutManager linerLayoutManager = new LinerLayoutManager(this);
+        recyclerView.setLayoutManager(linerLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        adapter = new TaskRVadapter(lstVCL);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
 
-                    @Override
-                    public void onDataChange(
-                            @NonNull DataSnapshot snapshot
-                    ) {
-
-                        lstVCL.clear();
-
-                        for (DataSnapshot obj :
-                                snapshot.getChildren()) {
-
-                            TASKS task =
-                                    obj.getValue(TASKS.class);
-
-                            if (task != null) {
-
-                                lstVCL.add(task);
-
-                                Log.w(
-                                        "VCL APP",
-                                        "Tên việc cần làm: "
-                                                + task.getName()
-                                );
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(
-                            @NonNull DatabaseError error
-                    ) {
-                    }
-                }
-        );
     }
+    //
+    new ValueEventListener() {
+
+        @Override
+        public void onDataChange(
+                @NonNull DataSnapshot snapshot
+                    ) {
+
+            lstVCL.clear();
+
+            for (DataSnapshot obj :
+                    snapshot.getChildren()) {
+
+                TASKS task =
+                        obj.getValue(TASKS.class);
+
+                if (task != null) {
+
+                    lstVCL.add(task);
+
+                    Log.w(
+                            "VCL APP",
+                            "Tên việc cần làm: "
+                                    + task.getName()
+                    );
+                }
+            }
+        }
+
+        @Override
+        public void onCancelled(
+                @NonNull DatabaseError error
+                    ) {
+        }
+    }
+        );
 }
