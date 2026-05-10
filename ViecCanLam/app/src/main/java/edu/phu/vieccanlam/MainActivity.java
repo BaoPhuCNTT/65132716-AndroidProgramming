1e9e13cf513b37db470aa9afae262cc64029c8d7
BaoPhuCNTT <phu.ndb.65cntt@ntu.edu.vn>
2026-05-11 01:12:05 +0700

    TodoList App P2

--- ViecCanLam/app/src/main/java/edu/phu/vieccanlam/MainActivity.java
+++ ViecCanLam/app/src/main/java/edu/phu/vieccanlam/MainActivity.java
@@ -1,24 +1,77 @@
 package edu.phu.vieccanlam;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<TASKS> lstVCL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Tạo danh sách
+        lstVCL = new ArrayList<>();
+
+        // Kết nối Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference databaseReference =
+                database.getReference("TASKS");
+
+        // Lắng nghe dữ liệu
+        databaseReference.addValueEventListener(
+                new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(
+                            @NonNull DataSnapshot snapshot
+                    ) {
+
+                        lstVCL.clear();
+
+                        for (DataSnapshot obj :
+                                snapshot.getChildren()) {
+
+                            TASKS task =
+                                    obj.getValue(TASKS.class);
+
+                            if (task != null) {
+
+                                lstVCL.add(task);
+
+                                Log.w(
+                                        "VCL APP",
+                                        "Tên việc cần làm: "
+                                                + task.getName()
+                                );
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(
+                            @NonNull DatabaseError error
+                    ) {
+                    }
+                }
+        );
     }
 }
